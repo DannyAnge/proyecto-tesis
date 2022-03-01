a79d7b86063ec5c777a04a8b686700f5bd91fb01
cesaredm <cesaredmaradiaga@outlook.com>
2022-03-01 12:55:37 -0600

    .

--- src/index.js
+++ src/index.js
@@ -7,18 +7,20 @@ const app = express();
 
 app.set("port", process.env.PORT || 3000);
 //decirle al server donde estaran las vistas
-app.set("views", path.join(__dirname + "/views")); //C://xampp/htdocs/practicas/clases-danny/src/views
+app.set("views", path.join(__dirname + "/views")); 
 //dcirle al server que extencion usaran las vistas
 app.engine("html", require("ejs").renderFile);
 //decirle al server que motor de plantilla usara
 app.set("view engine", "ejs");
 
+//para recibir objetos json mediante post
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //archivos publicos
 app.use(express.static(path.join(__dirname + "/public")));
 
+//controladores (manejadores) de rutas
 app.use(require("./route/index"));
 app.use(require("./route/facturacion"));
 app.use(require("./route/productos"));
